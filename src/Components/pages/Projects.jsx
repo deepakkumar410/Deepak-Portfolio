6f2bf537535905f388c41d886552e770ac89f5cf
Deepak  Kumar <deepakrai880899@gmail.com>
2025-02-15 00:48:50 +0530

    Update Projects.jsx

--- src/Components/pages/Projects.jsx
+++ src/Components/pages/Projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../Css/projects.css";
+import "../Css/Projects.css";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
